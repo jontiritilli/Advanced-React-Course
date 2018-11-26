a477d827cfef15efda365c5c2386a77d1780ec82
Jonathan Tiritilli <jonathantiritilli@gmail.com>
2018-11-26 13:23:01 -0800

    added basic queries and mutations, yoga set up, graphql basic setup complete

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,12 +1,16 @@
-const mutations = {
-  createDog(parent, args, ctx, info) {
-    global.dogs = global.dogs || [];
-    console.log(args);
-    const newDog = { name: args.name };
-
-    global.dogs.push(newDog);
-    return newDog;
+const Mutations = {
+  async createItem(parent, args, ctx, info) {
+    // TODO check if logged in
+    const item = await ctx.db.mutation.createItem(
+      {
+        data: {
+          ...args,
+        },
+      },
+      info
+    );
+    return item;
   },
 };
 
-module.exports = mutations;
+module.exports = Mutations;
---
+++ sick-fits/frontend/pages/_document.js
@@ -0,0 +1,41 @@
+import Document, { Head, Main, NextScript } from 'next/document';
+import { ServerStyleSheet } from 'styled-components';
+
+// class MyDocument extends Document {
+//   static getInitialProps({ renderPage }) {
+//     const sheet = new ServerStyleSheet();
+//     const page = renderPage(App => props =>
+//       sheet.collectStyles(<App {...props} />)
+//     );
+//     const styleTags = sheet.getStyleElement();
+//     return { ...page, styleTags }
+//   }
+
+//   render() {
+//     return (
+//       <html>
+//         <Head>{this.props.styleTags}</Head>
+//         <body>
+//           <Main />
+//           <NextScript />
+//         </body>
+//       </html>
+//     );
+//   }
+// }
+class MyDocument extends Document {
+  static async getInitialProps (ctx) {
+    const sheet = new ServerStyleSheet();
+    const originalRenderPage = ctx.renderPage;
+    ctx.renderPage = () =>
+      originalRenderPage({
+        enhanceApp: App => props => sheet.collectStyles(<App {...props} />),
+      });
+    const initialProps = await Document.getInitialProps(ctx);
+    return {
+      ...initialProps,
+      styles: [...initialProps.styles, ...sheet.getStyleElement()],
+    };
+  }
+}
+export default MyDocument;
